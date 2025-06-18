a090573fe34ab479a8f01a2012454bc2d746299d
Hugz12 <hugo.laloy@gmail.com>
2025-06-18 10:58:43 +0200

    Feat: Création des fichiers pour gérer l'appel à la base de données, les models et un main pour tester

---
+++ app/lib/db/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String, index=True)
