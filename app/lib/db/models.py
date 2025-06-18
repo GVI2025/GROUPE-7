31587af0fa1b1f09c842830f0630136114c177d3
Victor Masseron <victor.masseron--lucianaz@ig2i.centralelille.fr>
2025-06-18 11:27:46 +0200

    WIP

--- app/lib/db/models.py
+++ app/lib/db/models.py
@@ -1,8 +1,23 @@
-from sqlalchemy import Column, Integer, String
+from uuid import uuid4
+from sqlalchemy import Column, String, Float, Date, Enum, Integer, Boolean, ForeignKey, Time
+
 from .database import Base
 
-class Client(Base):
-    __tablename__ = "clients"
+class Salle(Base):
+    __tablename__ = "salles"
 
-    id = Column(Integer, primary_key=True, index=True)
-    nom = Column(String, index=True)
+    id = Column(String, primary_key=True, default=lambda: str(uuid4()))
+    nom = Column(String, nullable=False)
+    capacite = Column(Integer, nullable=False)
+    localisation = Column(String, nullable=False)
+    disponible = Column(Boolean, default=True, nullable=True)
+
+class Reservation(Base):
+    __tablename__ = "reservations"
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid4()))
+    salle_id = Column(String, ForeignKey("salles.id"))
+    date = Column(Date, nullable=False)
+    heure = Column(Time, nullable=False)
+    utilisateur = Column(String, nullable=False)
+    commentaire = Column(String, nullable=True)
